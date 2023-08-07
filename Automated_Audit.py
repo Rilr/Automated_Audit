0cf190034f428db54e9d8a4637ec435e3d372e7c
Rilr <Jorgelschn@gmail.com>
2023-08-07 19:18:58 -0400

    Genericized inputs for diffChecker()
    Added dicts for manage and audit and removed any dependencies from audit_df.

--- Automated_Audit.py
+++ Automated_Audit.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 import pandas as pd
 from datetime import datetime, timedelta
 
-def diffChecker(audit_df, inventory_lib):
+def diffChecker(source_lib, inventory_lib):
     '''Cleans and compares data from two frames
     
     Args:
@@ -21,24 +21,28 @@ def diffChecker(audit_df, inventory_lib):
     Returns:
         New data frame with calculated comparision
     '''
-
-    # Port file_path to a DataFrame; based on file_type
+    # Port source_lib['file_path'] to a DataFrame; based on file_type
+    if source_lib['file_type'] == "excel":
+        source_df = pd.read_excel(source_lib['file_path'], sheet_name=source_lib['sheet_name'], header=source_lib['header'], usecols=[source_lib['config_name']], engine='openpyxl')
+    elif source_lib['file_type'] == "csv":
+        source_df = pd.read_csv(source_lib['file_path'], header=source_lib['header'], usecols=[source_lib['config_name']])
+    # Port inventory_lib['file_path'] to a DataFrame; based on file_type
     if inventory_lib['file_type'] == "excel":
         input_df = pd.read_excel(inventory_lib['file_path'], header=inventory_lib['header'], usecols=[inventory_lib['column']], engine='openpyxl')
     elif inventory_lib['file_type'] == "csv":
         input_df = pd.read_csv(inventory_lib['file_path'], header=inventory_lib['header'], usecols=[inventory_lib['column']])
     
     # Clean the DataFrames by removing extraneous data and uppercase conversion
-    audit_df = audit_df.dropna(how='all')
-    audit_df[inventory_lib['config_name']] = audit_df[inventory_lib['config_name']].str.upper()
-    audit_df[inventory_lib['config_name']] = audit_df[inventory_lib['config_name']].replace(r"^ +| +$", r"", regex=True)
+    source_df = source_df.dropna(how='all')
+    source_df[inventory_lib['config_name']] = source_df[inventory_lib['config_name']].str.upper()
+    source_df[inventory_lib['config_name']] = source_df[inventory_lib['config_name']].replace(r"^ +| +$", r"", regex=True)
     input_df[inventory_lib['device_name']] = input_df[inventory_lib['device_name']].str.upper()
     input_df[inventory_lib['device_name']] = input_df[inventory_lib['device_name']].replace(r"^ +| +$", r"", regex=True)
     
     # Merge, compare and label the data
-    diffinput_df = pd.merge(audit_df, input_df, left_on=inventory_lib['config_name'], right_on=inventory_lib['device_name'], how='outer', suffixes=['_audit', f'_{inventory_lib["inv_system"]}'], indicator=True)
+    diffinput_df = pd.merge(source_df, input_df, left_on=source_lib['config_name'], right_on=inventory_lib['device_name'], how='outer', suffixes=[f'_{source_lib["inv_system"]}', f'_{inventory_lib["inv_system"]}'], indicator=True)
     diffinput_df = diffinput_df.rename(columns={'_merge': 'Found In'})
-    diffinput_df['Found In'] = diffinput_df['Found In'].replace({'left_only': 'Only in AUDIT', 'right_only': f'Only in {inventory_lib["inv_system"].upper()}'})
+    diffinput_df['Found In'] = diffinput_df['Found In'].replace({'left_only': f'Only in {source_lib["inv_system"].upper()}', 'right_only': f'Only in {inventory_lib["inv_system"].upper()}'})
     
     return diffinput_df
 
@@ -86,18 +90,27 @@ def main():
     layout = [[sg.Text('Audit XLSX')],
             [sg.InputText(key='fpaudit'),
             sg.FileBrowse(file_types=[("xlsx Files","*.xlsx")])],
+            
+            [sg.Text('Manage CSV')],
+            [sg.InputText(key="fpmanage"),
+            sg.FileBrowse(file_types=[("CSV Files","*.csv")])],
+            
             [sg.Text('Automate XLSX')],
             [sg.InputText(key="fpauto"),
             sg.FileBrowse(file_types=[("xlsx Files","*.xlsx")])],
+            
             [sg.Text('Intune CSV')],
             [sg.InputText(key="fpintune"),
             sg.FileBrowse(file_types=[("CSV Files","*.csv")])],
+            
             [sg.Text('Webroot CSV')],
             [sg.InputText(key="fpwebroot"),
             sg.FileBrowse(file_types=[("CSV Files","*.csv")])],
+            
             [sg.Text('File Output Location')],
             [sg.InputText(key="outpath"),
             sg.FolderBrowse()],
+            
             [sg.Button("Submit"),sg.Cancel()]],
     window = sg.Window('Auto-Automate by JLS', layout)
 
@@ -109,11 +122,32 @@ def main():
             break
 
         # Declare static variables used in functions
-        audit_file_path = values['fpaudit']
-        audit_df = pd.read_excel(audit_file_path, sheet_name=0, header=2, usecols=["Configuration Name"], engine='openpyxl')
+        #audit_file_path = values['fpaudit']
         auto_audit_out = values['outpath'] + '/audit-discrepancies.xlsx'
 
         # Define libraries
+        auditLib = {
+            "inv_system": "audit",
+            "config_name": "Configuration Name",
+            "sheet_name": 0,
+            "file_type": "excel",
+            "header": 2,
+            "column": "Configuration Name",
+            "file_path": values['fpaudit'],
+            "check_in": "Last Contact"
+        }
+
+        manageLib = {
+            "inv_system": "manage",
+            "config_name": "Configuration Name",
+            "device_name": "Device name",
+            "file_type": "csv",
+            "header": 0,
+            "column": 1,
+            "file_path": values['fpmanage'],
+            "check_in": "Last check-in"
+        }
+
         autoLib = {
             "inv_system": "auto",
             "config_name": "Configuration Name",
@@ -123,7 +157,6 @@ def main():
             "column": 0,
             "file_path": values['fpauto'],
             "check_in": "Last Contact"
-
         }
 
         intuneLib = {
@@ -151,12 +184,14 @@ def main():
         # Calls on the above functions and libraries to write a processed DataFrame into an .xlsx sheet; dependant on the file's presence
         if event == "Submit":
             with pd.ExcelWriter(auto_audit_out, mode='w', engine='xlsxwriter') as writer:
-                if audit_file_path and autoLib['file_path']:
-                    diffChecker(audit_df, autoLib,).to_excel(writer, sheet_name='diffAuto', index=False)
-                if audit_file_path and intuneLib['file_path']:
-                    diffChecker(audit_df, intuneLib).to_excel(writer, sheet_name='diffIntune', index=False)
-                if audit_file_path and webrootLib['file_path']:
-                    diffChecker(audit_df, webrootLib).to_excel(writer, sheet_name='diffWebroot', index=False)
+                if auditLib['file_path'] and autoLib['file_path']:
+                    diffChecker(auditLib, autoLib,).to_excel(writer, sheet_name='Audit v Auto', index=False)
+                if auditLib['file_path'] and intuneLib['file_path']:
+                    diffChecker(auditLib, intuneLib).to_excel(writer, sheet_name='Audit v Intune', index=False)
+                if auditLib['file_path'] and webrootLib['file_path']:
+                    diffChecker(auditLib, webrootLib).to_excel(writer, sheet_name='Audit v Webroot', index=False)
+                if manageLib['file_path'] and autoLib['file_path']:
+                    diffChecker(manageLib, autoLib).to_excel(writer, sheet_name='Manage v Auto', index=False)
                 if autoLib['file_path']:
                     dateChecker(autoLib).to_excel(writer, sheet_name='dateAuto', index=False)
                 if intuneLib['file_path']:
